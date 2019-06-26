e23c10906342464a5fbd050735eec137847a96ba
marija.taseva <marija.taseva@gowi.rs>
2019-06-26 20:14:47 +0200

    Added bar,line and pie charts

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+const SAMPLE_BARCHART_DATA: any[] = [
+  {data: [1, 58, 84, 78, 5, 56, 45], label:'Q3 Sales'},
+  {data: [77, 58, 43, 55, 52, 12, 89], label:'Q4 Sales'},
+]
+
+const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7']
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  public barChartData: any[] = SAMPLE_BARCHART_DATA;
+  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartOptions: any ={
+    scaleShowVericalLines: false,
+    responsive: true
+  };
+
+  ngOnInit() {
+  }
+
+}
