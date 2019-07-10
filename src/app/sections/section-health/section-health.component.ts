f98f0dcd264ffcae673048f79d6a249e9a01ba2b
marija.taseva <marija.taseva@gowi.rs>
2019-07-10 21:05:49 +0200

    Created server sevice and component working with API

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,25 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Server } from '../../shared/server';
+import { ServerMessage } from '../../shared/server-message';
+import { ServerService } from 'src/app/services/server.service';
+import { Observable } from 'rxjs/Rx';
+import { AnonymousSubscription } from 'rxjs/Subscription';
 
-const SAMPLE_SERVERS = [
-  { id: 1, name: 'dev-web', isOnline: false },
-  { id: 2, name: 'dev-mail', isOnline: true },
-  { id: 3, name: 'dev-web', isOnline: false },
-  { id: 4, name: 'dev-web', isOnline: true }
-]
+//const SAMPLE_SERVERS = [
+//  { id: 1, name: 'dev-web', isOnline: false },
+//  { id: 2, name: 'dev-mail', isOnline: true },
+//  { id: 3, name: 'dev-web', isOnline: false },
+//  { id: 4, name: 'dev-web', isOnline: true }
+//]
 
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.scss']
 })
-export class SectionHealthComponent implements OnInit {
+export class SectionHealthComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private _serverService: ServerService) { }
 
-  servers: Server[] = SAMPLE_SERVERS;
+  servers: Server[];
+  timerSubscription: AnonymousSubscription;
 
   ngOnInit() {
+    this.refreshData();
   }
 
+  ngOnDestroy() {
+    if(this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  refreshData() {
+    this._serverService.getServers().subscribe(res => {
+      this.servers = res;
+    });
+
+    this.subscribeToData();
+  }
+
+  subscribeToData() {
+    this.timerSubscription = Observable.timer(5000).first().subscribe(() => this.refreshData());
+  }
+
+  sendMessage(msg: ServerMessage) {
+    this._serverService.handleServerMessage(msg).subscribe(res => console.log('Message sent to server: ', msg), err => console.log('Error: ', err));
+  }
 }
